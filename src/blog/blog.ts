945b190c7fbe28b3d24b092d7f6413070231620b
carbage <carbage@github.com>
2025-08-05 19:54:20 +0100

    Recursive blog posts

--- src/blog/blog.ts
+++ src/blog/blog.ts
@@ -20,7 +20,7 @@ function getMarkdownFiles(dir: string): string[] {
   return fs.readdirSync(dir).flatMap((file) => {
     const fullPath = path.join(dir, file);
     if (fs.statSync(fullPath).isDirectory()) {
-      return getMarkdownFiles(fullPath).map((f) => path.relative(POSTS_DIR, f));
+      return getMarkdownFiles(fullPath);
     }
     return file.endsWith(".md") ? [path.relative(POSTS_DIR, fullPath)] : [];
   });
