15cec7bd740bb2efb07f6e9f048083a76f58ba81
Aiden Harwood <carbage@github.com>
2025-09-08 13:08:37 +0100

    Optimize terminal

--- src/k9s/k9s.ts
+++ src/k9s/k9s.ts
@@ -91,121 +91,115 @@ async function createK9sPod() {
 export function createWsServer(server: Server) {
   const wss = new WebSocketServer({ server, path: "/k9s" });
 
-  wss.on("connection", (ws: WebSocket) => {
+  wss.on("connection", async (ws: WebSocket) => {
     ws.binaryType = "arraybuffer";
     try {
       let closed = false;
 
-      // Expect an init message first to choose pod/namespace/container/cmd
-      ws.once("message", async (firstMsg: string | Buffer) => {
-        const stdinStream = new PassThrough();
-        const stdoutStream = new PassThrough();
-        const stderrStream = new PassThrough();
+      const stdinStream = new PassThrough();
+      const stdoutStream = new PassThrough();
+      const stderrStream = new PassThrough();
 
-        const shutdown = (reason: string) => {
-          console.log("Shutting down k9s session: ", reason);
-          ws.send(`\r\nSession terminated: ${reason}\r\n`);
-          if (!closed) {
-            stdinStream.end();
-            stderrStream.end();
-            stdoutStream.end();
-            if (pod) deletePod(pod);
-            try {
-              if (ws.readyState === ws.OPEN) ws.close();
-            } catch {}
-          }
-          closed = true;
-        };
-
-        ws.send(`Creating pod...\r\n`);
-
-        console.log("Creating pod...");
-        const pod: V1Pod | undefined = await createK9sPod();
-
-        if (!pod) {
-          shutdown("Could not create k9s pod");
-          return;
-        } else {
-          ws.send(
-            `Created pod ${pod.metadata?.name} created, waiting to start...\r\n`
-          );
-          console.log(
-            `Created pod ${pod.metadata?.name} created, waiting to start...`
-          );
-          // wait up to a minute for pod to be running
-          const statusCheckStart = Date.now();
-          while (
-            (await getPodStatus(pod))?.status?.containerStatuses?.[0].state
-              ?.running === undefined
-          ) {
-            if (Date.now() - statusCheckStart > 60000) {
-              shutdown("Timed out waiting for k9s pod to start");
-              return;
-            }
-            await new Promise((r) => setTimeout(r, 1000));
-          }
+      const shutdown = (reason: string) => {
+        console.log("Shutting down k9s session: ", reason);
+        ws.send(`\r\nSession terminated: ${reason}\r\n`);
+        if (!closed) {
+          stdinStream.end();
+          stderrStream.end();
+          stdoutStream.end();
+          if (pod) deletePod(pod);
+          try {
+            if (ws.readyState === ws.OPEN) ws.close();
+          } catch {}
         }
+        closed = true;
+      };
 
-        // forward stdout/stderr to websocket
-        stdoutStream.on("data", (chunk: Buffer) => {
-          const str = chunk.toString("utf8");
-          console.log("Received stdout len=%d repr=%j", chunk.length, str);
-          if (ws.readyState === ws.OPEN) ws.send(chunk);
-        });
-        stderrStream.on("data", (chunk: Buffer) => {
-          const str = chunk.toString("utf8");
-          console.log("Received stdout len=%d repr=%j", chunk.length, str);
-          if (ws.readyState === ws.OPEN) ws.send(chunk);
-        });
+      ws.send(`Creating pod...\r\n`);
 
-        // forward websocket messages to stdin
-        ws.on("message", (m) => {
-          if (closed) return;
-          if (Buffer.isBuffer(m)) stdinStream.write(m);
-          else stdinStream.write(Buffer.from(String(m)));
-        });
+      console.log("Creating pod...");
+      const pod: V1Pod | undefined = await createK9sPod();
 
-        ws.on("close", () => shutdown("websocket closed"));
-        ws.on("error", (e) =>
-          shutdown("websocket error: " + JSON.stringify(e))
+      if (!pod) {
+        shutdown("Could not create k9s pod");
+        return;
+      } else {
+        ws.send(
+          `Created pod ${pod.metadata?.name} created, waiting to start...\r\n`
         );
-
-        // const cmd = ["k9s"];
-        // const cmd = ["/bin/sh"];
-        const cmd = [
-          "/bin/sh",
-          "-lc",
-          "apk add shadow bash acl; mkdir -p /home/guest; setfacl -m u:guest:rwx /home/guest; usermod -d /home/guest guest; chsh -s /bin/bash guest; su guest",
-        ];
-
-        try {
-          ws.send(`Attempting to attach...\r\n`);
-          console.log("Attempting to attach...");
-          const attach = await createExec();
-          if (!attach) {
-            shutdown("Could not create k8s attach");
+        console.log(
+          `Created pod ${pod.metadata?.name} created, waiting to start...`
+        );
+        // wait up to a minute for pod to be running
+        const statusCheckStart = Date.now();
+        while (
+          (await getPodStatus(pod))?.status?.containerStatuses?.[0].state
+            ?.running === undefined
+        ) {
+          if (Date.now() - statusCheckStart > 60000) {
+            shutdown("Timed out waiting for k9s pod to start");
             return;
           }
-          attach
-            .exec(
-              pod.metadata?.namespace ?? "",
-              pod.metadata?.name ?? "",
-              pod.spec?.containers?.[0].name ?? "",
-              cmd,
-              stdoutStream,
-              stderrStream,
-              stdinStream,
-              true, // tty
-              () => shutdown("k9s attach closed")
-            )
-            .then(() => {
-              ws.send(`Attached! Welcome to k9s\r\n`);
-              console.log("Attached! Welcome to k9s");
-            }, undefined);
-        } catch (err: any) {
-          shutdown("attach error: " + JSON.stringify(err));
+          await new Promise((r) => setTimeout(r, 1000));
         }
+      }
+
+      // forward stdout/stderr to websocket
+      stdoutStream.on("data", (chunk: Buffer) => {
+        const str = chunk.toString("utf8");
+        console.log("Received stdout len=%d repr=%j", chunk.length, str);
+        if (ws.readyState === ws.OPEN) ws.send(chunk);
       });
+      stderrStream.on("data", (chunk: Buffer) => {
+        const str = chunk.toString("utf8");
+        console.log("Received stdout len=%d repr=%j", chunk.length, str);
+        if (ws.readyState === ws.OPEN) ws.send(chunk);
+      });
+
+      // forward websocket messages to stdin
+      ws.on("message", (m) => {
+        if (closed) return;
+        if (Buffer.isBuffer(m)) stdinStream.write(m);
+        else stdinStream.write(Buffer.from(String(m)));
+      });
+
+      ws.on("close", () => shutdown("websocket closed"));
+      ws.on("error", (e) => shutdown("websocket error: " + JSON.stringify(e)));
+
+      // const cmd = ["k9s"];
+      // const cmd = ["/bin/sh"];
+      const cmd = [
+        "/bin/sh",
+        "-lc",
+        "apk add --quiet --no-progress shadow bash acl; mkdir -p /home/guest; setfacl -m u:guest:rwx /home/guest; usermod -d /home/guest guest; chsh -s /bin/bash guest; su -l guest",
+      ];
+
+      try {
+        ws.send(`Attempting to attach...\r\n`);
+        console.log("Attempting to attach...");
+        const attach = await createExec();
+        if (!attach) {
+          shutdown("Could not create k8s attach");
+          return;
+        }
+        attach
+          .exec(
+            pod.metadata?.namespace ?? "",
+            pod.metadata?.name ?? "",
+            pod.spec?.containers?.[0].name ?? "",
+            cmd,
+            stdoutStream,
+            stderrStream,
+            stdinStream,
+            true, // tty
+            () => shutdown("k9s attach closed")
+          )
+          .then(() => {
+            ws.send(`Attached!\r\n`);
+          }, undefined);
+      } catch (err: any) {
+        shutdown("attach error: " + JSON.stringify(err));
+      }
     } catch (err) {
       console.error("k9s websocket error:", err);
       try {
